fe46c757001f7f1229bcf7f68093925ec46626cc
Youssef <yousseffusa87@gmail.com>
2024-12-06 10:52:25 -0500

    length of the characters

--- Program.fs
+++ Program.fs
@@ -1,1 +1,16 @@
-﻿printfn "Hello from F#" // Hello
+﻿open System
+
+let getInputAndDisplayLength (inputText: string) =
+    printfn "The length of the input text is: %d" inputText.Length
+
+
+[<EntryPoint>]  
+let main argv =
+    printf "Please enter some text: "
+    let inputText = Console.ReadLine()
+    match inputText with
+    | null | "" ->
+        printfn "No input provided."
+    | _ ->
+        getInputAndDisplayLength inputText 
+    0
