e434d988bfb000766085c3a86ae694649b034c07
Youssef <yousseffusa87@gmail.com>
2024-12-06 11:09:25 -0500

    some comments

--- Program.fs
+++ Program.fs
@@ -11,7 +11,7 @@ let getSentenceCount (inputText: string) =
     let sentences = inputText.Split([|'.'; '!'; '?'|], StringSplitOptions.RemoveEmptyEntries)
     printfn "The number of sentences is: %d" sentences.Length
 
-[<EntryPoint>]  
+[<EntryPoint>]   // Entry Point that the program will start from.
 let main argv =
     printf "Please enter some text: "
     let inputText = Console.ReadLine()
@@ -19,8 +19,8 @@ let main argv =
     | null | "" ->
         printfn "No input provided."
     | _ ->
-        getInputAndDisplayLength inputText 
-        getWordCount inputText
-        getSentenceCount inputText
+        getInputAndDisplayLength inputText  // lenght of the characters
+        getWordCount inputText              // number of the words
+        getSentenceCount inputText          // number of the sentences
 
     0
